019f6a2d0bc68d993e56253f6726a79923de0c99
OGNeekoPolo <nktrabue@gmail.com>
2017-09-19 01:09:24 -0400

    Done?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,8 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
-    }
+    },
+    :url => ':s3_domain_url'
   }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
